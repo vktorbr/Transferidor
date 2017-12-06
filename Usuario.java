25c2afaec846d12c8fdca243a73b6b23ad649545
vms101 <33494087+vms101@users.noreply.github.com>
2017-12-06 05:52:54 -0300

    Add files via upload

--- Usuario.java
+++ Usuario.java
@@ -18,10 +18,10 @@ import javax.swing.JLabel;
 public class Usuario extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField ipServidor;
+	private JTextField portaCliente;
 	public Cliente cliente;
+	private JTextField PortaServidor;
 	/**
 	 * Launch the application.
 	 */
@@ -42,33 +42,30 @@ public class Usuario extends JFrame {
 
 	public Usuario() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 633, 445);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(76, 16, 112, 23);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		ipServidor = new JTextField();
+		ipServidor.setBounds(475, 16, 112, 23);
+		contentPane.add(ipServidor);
+		ipServidor.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(271, 16, 112, 23);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(76, 45, 112, 23);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		portaCliente = new JTextField();
+		portaCliente.setBounds(103, 46, 112, 23);
+		contentPane.add(portaCliente);
+		portaCliente.setColumns(10);
 		JButton btnNewButton = new JButton("Iniciar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
 				//textPane.setText(textField.getText());
 				
 				try {
-					cliente = new Cliente("localhost", 3000);
+					cliente = new Cliente(ipServidor.getText(), Integer.parseInt(PortaServidor.getText()));
+					
 					Thread client;
 	                client = new Thread(cliente);
 	                client.start();
@@ -81,7 +78,7 @@ public class Usuario extends JFrame {
 				
 			}
 		});
-		btnNewButton.setBounds(312, 129, 89, 23);
+		btnNewButton.setBounds(266, 16, 89, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Cancelar");
@@ -95,7 +92,7 @@ public class Usuario extends JFrame {
 				}
 			}
 		});
-		btnNewButton_1.setBounds(312, 227, 89, 23);
+		btnNewButton_1.setBounds(266, 112, 89, 23);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Pausar");
@@ -104,7 +101,7 @@ public class Usuario extends JFrame {
 				cliente.setPausado(true);
 			}
 		});
-		btnNewButton_2.setBounds(312, 160, 89, 23);
+		btnNewButton_2.setBounds(266, 46, 89, 23);
 		contentPane.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Reiniciar");
@@ -113,7 +110,7 @@ public class Usuario extends JFrame {
 				cliente.setPausado(false);
 			}
 		});
-		btnNewButton_3.setBounds(312, 193, 89, 23);
+		btnNewButton_3.setBounds(266, 78, 89, 23);
 		contentPane.add(btnNewButton_3);
 		
 		JTextPane textPane = new JTextPane();
@@ -125,19 +122,32 @@ public class Usuario extends JFrame {
 		contentPane.add(lblIp);
 		
 		JLabel lblNewLabel = new JLabel("CLIENTE");
-		lblNewLabel.setBounds(198, 0, 46, 14);
+		lblNewLabel.setBounds(384, 0, 46, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblIpDoCliente = new JLabel("IP do Cliente:");
-		lblIpDoCliente.setBounds(10, 20, 74, 14);
+		lblIpDoCliente.setBounds(10, 20, 103, 14);
 		contentPane.add(lblIpDoCliente);
 		
 		JLabel lblNewLabel_1 = new JLabel("IP do Servidor:");
-		lblNewLabel_1.setBounds(198, 20, 74, 14);
+		lblNewLabel_1.setBounds(384, 20, 103, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblPorta = new JLabel("Porta:");
-		lblPorta.setBounds(42, 50, 46, 14);
+		JLabel lblPorta = new JLabel("Porta Cliente:");
+		lblPorta.setBounds(10, 50, 89, 14);
 		contentPane.add(lblPorta);
+		
+		PortaServidor = new JTextField();
+		PortaServidor.setBounds(475, 46, 112, 23);
+		contentPane.add(PortaServidor);
+		PortaServidor.setColumns(10);
+		
+		JLabel lblPortaServidor = new JLabel("Porta Servidor:");
+		lblPortaServidor.setBounds(384, 50, 103, 14);
+		contentPane.add(lblPortaServidor);
+		
+		JTextPane ipCliente = new JTextPane();
+		ipCliente.setBounds(103, 19, 112, 20);
+		contentPane.add(ipCliente);
 	}
 }
